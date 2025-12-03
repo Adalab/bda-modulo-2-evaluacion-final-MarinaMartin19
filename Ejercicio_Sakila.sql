a0129cd516a5a9928b337f233e7cb56f32a44e8c
MarinaMartin19 <mmartin.diaz19@gmail.com>
2025-12-03 13:58:26 +0100

    Exercise 2 finished

--- Ejercicio_Sakila.sql
+++ Ejercicio_Sakila.sql
@@ -50,3 +50,36 @@ FROM customer c
 JOIN rental r ON c.customer_id = r.customer_id
 GROUP BY c.customer_id, c.first_name, c.last_name
 ORDER BY total_alquileres DESC;
+
+-- 11. Cantidad total de películas alquiladas por categoría (nombre categoría + recuento).
+SELECT cat.name AS categoria,
+COUNT(r.rental_id) AS total_alquileres
+FROM category cat
+JOIN film_category fc ON cat.category_id = fc.category_id
+JOIN inventory i ON fc.film_id = i.film_id
+JOIN rental r ON i. inventory_id = r.inventory_id
+GROUP BY cat.category_id, cat.name
+ORDER BY total_alquileres DESC;
+
+-- 12. Promedio de duración de las películas para cada clasificación (film).
+SELECT rating, AVG(length) AS promedio_duracion
+FROM film
+GROUP BY rating;
+
+-- 13. Nombre y apellido de los actores que aparecen en la película con title 'Indian Love'.
+SELECT a.first_name, a.last_name
+FROM film f
+JOIN film_actor fa ON f.film_id = fa.film_id
+JOIN actor a ON fa.actor_id = a.actor_id
+WHERE f.title = 'Indian love';
+
+-- 14. Título de todas las películas que contengan la palabra 'dog' o 'cat' en su descripción.
+SELECT title, description
+FROM film
+WHERE description LIKE '%dog%'
+OR description LIKE '%cat%';
+
+-- 15. Título de las películas que fueron lanzadas entre el año 2005 y 2010.
+SELECT title, release_year
+FROM film
+WHERE release_year BETWEEN 2005 AND 2010;
